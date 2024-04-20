65c189be6694077b1676394795e991123169d3ec
norhan karm <142645218+Nourhankarm@users.noreply.github.com>
2024-04-21 00:49:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,36 +13,19 @@ model_file_path = 'diabetes_model.sav'
 with open(model_file_path, 'rb') as file:
     model = pickle.load(file)
 
-# Correct the syntax error in the path and load the encoder
-encoder_file_path = 'encoder.sav'  # Corrected path
-with open(encoder_file_path, 'rb') as file:
-    encoder = pickle.load(file)
-
 # API endpoint for predictions
 @app.route('/predict', methods=['POST'])
 def get_detections():
-    try:
-        data = request.json
-        print(data)
-        gender = data['gender']
-        age = data['age']
-        hypertension = data['hypertension']
-        heart_disease =data['heart_disease']
-        smoking_history = data['smoking_history']
-        bmi = data['bmi']
-        HbA1c_level = data['HbA1c_level']
-        blood_glucose_level = data['blood_glucose_level'] 
-        print(f"data received: gender= {data['gender']}, age= {age}, hypertension= {hypertension}, heart_disease= {heart_disease}, smoking_history= {data['smoking_history']}, bmi= {bmi}, HbA1c_level= {HbA1c_level}, blood_glucose_level= {blood_glucose_level}")
-        #  'gender' and 'smoking_history' are categorical and need to be encoded
+    data = request.get_json(force=True)
+    print(data["gender"])
+    features = [data['gender'], data['age'], data['hypertension'], data['heart_disease'],
+                data['smoking_history'], data['bmi'], data['HbA1c_level'], data['blood_glucose_level']]
+    prediction = rf.predict([features])
+    return jsonify({'diabetes_prediction': int(prediction[0])})
+
+     print(f"data received: gender= {data['gender']}, age= {age}, hypertension= {hypertension}, heart_disease= {heart_disease}, smoking_history= {data['smoking_history']}, bmi= {bmi}, HbA1c_level= {HbA1c_level}, blood_glucose_level= {blood_glucose_level}")
+        #gender' and 'smoking_history' are categorical and need to be encoded
         print(encoder.classes_)
-        encoded_smoking_history = encoder.transform([smoking_history])[0]
-        print(encoded_smoking_history)
-        if(gender=="Male"):
-            encoded_gender=1
-        elif (gender=="Female"):
-            encoded_gender=0
-        else:
-            encoded_gender=2
         
         
 
@@ -61,12 +44,6 @@ def get_detections():
 
     except FileNotFoundError:
         abort(404)
-
-
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
-
-
 
 
 if __name__ == '__main__':
